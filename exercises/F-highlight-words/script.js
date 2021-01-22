3353f559dbfce789c455257cd856e45dadd56e9f
Miss <emesquitta73@gmail.com>
2021-01-22 17:45:23 +0000

    completed F

--- exercises/F-highlight-words/script.js
+++ exercises/F-highlight-words/script.js
@@ -1,29 +1,31 @@
 function highlightWords(paragraph, colours) {
-  function listOfColours(colours) {
     let content = document.querySelector("#content");
     let select = document.createElement("select");
-    let pea = document.createElement("h1");
+    let pee = document.createElement("p");
+    let words = paragraph.split(" ");
+      words.forEach(element => {
+    let span = document.createElement("span");
+      span.innerText = `${element} `;
+      pee.appendChild(span);
+      span.addEventListener("click", event =>{
+         event.preventDefault();
+    let colour = select.value;
+      span.style.background = `${colour}`;
+      })
+    })
     colours.forEach(element => {
-      let option = document.createElement("option");
+    let option = document.createElement("option");
       option.value = element;
       option.innerText = element;
       select.appendChild(option);
-  select.addEventListener("click", event =>{
-        event.preventDefault();
-        console.log(event.target.value);
-        if(event.target.value === element){
-         pea.innerHTML = `you have selected: ${element}`
-        }
-      })
+   
     })
-  content.appendChild(pea);
+    content.appendChild(pee);
     content.appendChild(select);
   }
-}
 
 const paragraph =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis massa ut sem finibus ultrices. Phasellus hendrerit placerat libero non facilisis. Etiam tincidunt ut elit id elementum. Etiam accumsan semper ipsum, ac porttitor ex tempus non. Donec vitae massa condimentum, faucibus magna non, sagittis libero. Phasellus ullamcorper malesuada tellus at egestas. Duis volutpat turpis velit, ut bibendum tellus ornare ac. Nullam feugiat nisi fringilla eleifend scelerisque. Phasellus elit nulla, sodales eget consectetur sed, tincidunt vitae velit. Fusce eget tempus magna, vitae ultrices purus. Proin ac purus tellus. Curabitur rhoncus est quis libero egestas ultrices. Proin viverra a ex sed convallis. Fusce ut orci consectetur, placerat ipsum non, commodo neque. Nulla at dui velit. In ut accumsan libero. Duis ac lacus consectetur, posuere eros nec, aliquet nisl. Quisque sodales fringilla lacinia. Donec hendrerit ornare lectus, nec iaculis justo tincidunt et. Cras a varius libero, ac bibendum est. Aliquam molestie, leo non aliquam lobortis, nulla tortor hendrerit turpis, et ullamcorper magna diam quis elit. Maecenas elit libero, tincidunt sit amet lacinia sit amet, venenatis nec est.";
-
 const colours = ["yellow", "green", "blue", "none"];
 
 highlightWords(paragraph, colours);
